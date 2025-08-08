bc3069b756efe2d08d91a89020f206d46ea8613e
SSIsa14 <sandovalisabela0524@gmail.com>
2025-08-08 14:35:29 -0600

    Ambientes de dev, qa y prod funcionando individualmente

--- pharmacy/src/main/java/com/example/pharmacy/config/SecurityConfig.java
+++ pharmacy/src/main/java/com/example/pharmacy/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4300"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Skip-Jwt-Interceptor"));
         configuration.setExposedHeaders(List.of("Authorization"));
